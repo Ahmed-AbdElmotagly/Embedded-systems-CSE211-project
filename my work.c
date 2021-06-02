d6b6e9a3053309e7eb3f786eb16ff1cc439eabbb
Ahmed-AbdElmotagly <84779570+Ahmed-AbdElmotagly@users.noreply.github.com>
2021-06-02 12:32:21 +0200

    Update my work.c

--- my work.c
+++ my work.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
-#include "tm4c123gh6pm.h"
+#include <tm4c123gh6pm.h>
+void SystemInit () {} ;
 
-void port_Init (void) {                  // this function is to activate the ports which will be used inshaa allah
-SYSCTL_RCGCGPIO_R = 0x20 ;               // to activate port F bs mtl3 error w na l sra7a 3wz anam
-while ((SYSCTL_PRGPIO_R & 0x20)==0){}    // waiting for the activation 
-GPIO_PORTF_LOCK_R = 0x4C4F434B ;         // UNLOCKING PORT F 
+void port_Init (void) {                     // this function is to activate the ports which will be used inshaa allah
+	SYSCTL_RCGCGPIO_R = 0x20 ;                // to activate port F 
+	while ((SYSCTL_PRGPIO_R & 0x20)==0){}     // waiting for the activation 
+	GPIO_PORTF_LOCK_R = 0x4C4F434B ;          // UNLOCKING PORT F 
+}
+// ##################################################################################################################################
+void LED_Init (void) {                    	// this function is to unlock the pins and enable thhem as digital output
+	GPIO_PORTF_DATA_R   = 0x02 ;                 // on 
+	GPIO_PORTF_PUR_R		= 0x02 ;                 // use pull up resistance
+	GPIO_PORTF_CR_R  		= 0x02 ;                 // unlock pin F1 (the red led )
+	GPIO_PORTF_DIR_R 		= 0x02 ;                 // pin F1 as output
+	GPIO_PORTF_DEN_R 		= 0x02 ;                 // to enable digital
+	GPIO_PORTF_AFSEL_R 	= ~(0x02);               // disable Alternate function select 
+	GPIO_PORTF_AMSEL_R 	= ~(0x02);               // disable Analog
+	GPIO_PORTF_PCTL_R 	= 0x00000000; 					 // regular digital function	
+}
+// ####################################################################################################################################
+int main() {
+	
 }
 
 
@@ -19,3 +35,4 @@ GPIO_PORTF_LOCK_R = 0x4C4F434B ;         // UNLOCKING PORT F
 #define GPIO_PORTF_LOCK_R         (*((volatile unsigned long*)0x40025520))
 #define SYSCTL_RCGCGPIO_R         (*((volatile unsigned long*)0x400FE608))
 #define SYSCTL_PRGPIO_R           (*((volatile unsigned long*)0x400FEA08)) */
+
