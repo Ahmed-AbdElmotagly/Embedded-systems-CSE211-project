a486d4902c68cc7fb5d4edfa3e8260becf72057e
Ahmed-Mohamed-Hassan <85200141+Ahmed-Mohamed-Hassan@users.noreply.github.com>
2021-06-06 03:16:30 +0200

    Update my work.c

--- my work.c
+++ my work.c
@@ -18,10 +18,11 @@
 
 void SystemInit () {} ;
 
-void port_Init (void) {                    	 // this function is to activate the ports which will be used inshaa allah
-	SYSCTL_RCGCGPIO_R = 0x20 ;                // to activate port F 
-	while ((SYSCTL_PRGPIO_R & 0x20)==0){}     // waiting for the activation 
+void port_Init (void) {                     // this function is to activate the ports which will be used inshaa allah
+	SYSCTL_RCGCGPIO_R = 0x37 ;                // to activate port F , E,C,B,A 
+	while ((SYSCTL_PRGPIO_R & 0x37)==0){}     // waiting for the activation 
 	GPIO_PORTF_LOCK_R = 0x4C4F434B ;          // UNLOCKING PORT F 
+		
 }
 // ##################################################################################################################################
 void  LED_Init (char data) {                   		    	  // this function is to turn on Led using char for 8 bits
@@ -35,6 +36,26 @@ void  LED_Init (char data) {                   		    	  // this function is to t
 	GPIO_PORTF_PCTL_R 		&= 0x00000000;            // disable special functions
 }
 // ####################################################################################################################################
+// port A ===> (A5 , A6 , A7) for controll
+// A5 --> select register    ,  A6 ----> r/w , A7 ---->     
+// Port B ====> For data D0 ===> D7
+
+// 2 functions for Delay 
+void delayMs(int ms){
+
+    int i , j ;
+    for(i=0;i<ms;i++)
+    for(j=0;j<3180;j++)
+    {} // delay for 1 Ms
+}
+void delayUs(int us){
+
+    int i , j ;
+    for(i=0;i<us;i++)
+    for(j=0;j<3;j++)
+    {} 
+}	
+
 void	LCD_DATA (char DUAL){				// this function for the data to be placed on  pins of port B to connect the LCD
 	GPIO_PORTB_DATA_R   		|= DUAL ;       // connect the pins of the lcd to port B pins in the sequence PB0 to D0 ..
 	GPIO_PORTB_DIR_R 		|= ALL ;	// the 8 pins of port B as OUTPUT
@@ -110,16 +131,6 @@ int main() {
 	}
 }
 
-/*#define GPIO_PORTF_DATA_R         (*((volatile unsigned long*)0x400253FC))
-#define GPIO_PORTF_DIR_R          (*((volatile unsigned long*)0x40025400))
-#define GPIO_PORTF_AFSEL_R        (*((volatile unsigned long*)0x40025420))
-#define GPIO_PORTF_PUR_R          (*((volatile unsigned long*)0x40025510))
-#define GPIO_PORTF_DEN_R          (*((volatile unsigned long*)0x4002551C))
-#define GPIO_PORTF_CR_R           (*((volatile unsigned long*)0x40025524))
-#define GPIO_PORTF_AMSEL_R        (*((volatile unsigned long*)0x40025528))
-#define GPIO_PORTF_PCTL_R         (*((volatile unsigned long*)0x4002552C))
-#define GPIO_PORTF_LOCK_R         (*((volatile unsigned long*)0x40025520))
-#define SYSCTL_RCGCGPIO_R         (*((volatile unsigned long*)0x400FE608))
-#define SYSCTL_PRGPIO_R           (*((volatile unsigned long*)0x400FEA08)) */
+
 	
 
